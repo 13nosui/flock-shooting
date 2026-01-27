1c7792284abcddaf3996a646d9c5940130cfd22e
13nosui <13.no.sui@gmail.com>
2026-01-27 12:35:55 +0900

    balance: reduced Interceptor bullet speed to 30

--- src/Obstacle.js
+++ src/Obstacle.js
@@ -62,7 +62,7 @@ class VoxelObstacle {
                 // Determine Bullet Speed based on Enemy Type
                 let bulletSpeed = 10; // TANK (was 20)
                 if (this.type === 'NORMAL') bulletSpeed = 20; // NORMAL (was 50)
-                if (this.type === 'INTERCEPTOR') bulletSpeed = 40; // INTERCEPTOR (was 80)
+                if (this.type === 'INTERCEPTOR') bulletSpeed = 30; // INTERCEPTOR (was 40)
 
                 // Spawn position: Start slightly in front of the enemy to avoid clipping
                 let spawnZ = this.pos.z + (this.size / 2) + 20;
